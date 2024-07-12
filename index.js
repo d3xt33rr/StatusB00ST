3d0905a7d5acd02f72012983b38012a4b5d9b1e6
D3XT3R <106473446+d3xt33rr@users.noreply.github.com>
2024-07-12 17:05:02 +0200

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["PLAYING","MUSIC"];
+const statusMessages = ["discord.gg/b00stmarket","discord.gg/b00stmarket"];
 
 
 let currentIndex = 0;
